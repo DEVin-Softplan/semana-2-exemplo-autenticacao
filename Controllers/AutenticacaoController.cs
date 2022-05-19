542c9d12cc6aa38f336a9a7a3fa2a7802eec583d
Vitor Lima <vitor.lima@softplan.com.br>
2022-05-19 18:47:50 -0400

    Adiciona RefreshToken

--- Controllers/AutenticacaoController.cs
+++ Controllers/AutenticacaoController.cs
@@ -1,7 +1,9 @@
 using autenticacao.Dtos;
 using autenticacao.Repositories;
 using autenticacao.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace autenticacao.Controllers
 {
@@ -21,8 +23,42 @@ namespace autenticacao.Controllers
             if (user == null) return NotFound();
 
             var token = TokenService.GenerateToken(user);
+            var newRefreshToken = TokenService.GenerateRefreshToken();
+            TokenService.SaveRefreshToken(user.Username, newRefreshToken);
 
-            return Ok(new {token});
+            return Ok(new
+            {
+                token,
+                newRefreshToken
+            });
+
+        }
+
+        [HttpPost]
+        [Route("refresh")]
+        [AllowAnonymous]
+        public ActionResult<dynamic> RefreshToken([FromQuery] string token, [FromQuery] string refreshToken)
+        {
+
+            var principal = TokenService.GetPrincipalFromExpiredToken(token);
+            var username = principal.Identity.Name;
+            var savedRefreshToken = TokenService.GetRefreshToken(username);
+
+            if (savedRefreshToken != refreshToken)
+                throw new SecurityTokenException("Invalid refresh token");
+
+            var newToken = TokenService.GenerateToken(principal.Claims);
+            var newRefreshToken = TokenService.GenerateRefreshToken();
+            TokenService.DeleteRefreshToken(username, refreshToken);
+            TokenService.SaveRefreshToken(username, newRefreshToken);
+
+            return new ObjectResult(new
+            {
+                token = newToken,
+                refreshToken = newRefreshToken
+                
+            });
+
         }
 
     }
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using autenticacao.Models;
 using Microsoft.IdentityModel.Tokens;
@@ -13,18 +16,25 @@ namespace autenticacao.Services
 
         public static string GenerateToken(User user)
         {
+               var claims = new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                };
+
+            return GenerateToken(claims);
+        }
+
+
+        public static string GenerateToken(IEnumerable<Claim> claims)
+        {
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(Settings.Secret);
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
-                Subject = new ClaimsIdentity(new Claim[]
-                {
-                    new Claim(ClaimTypes.Name, user.Username),
-                    new Claim(ClaimTypes.Role, user.Role),
-                    new Claim("Projeto", "DevInHouse"),
-                }),
+                Subject = new ClaimsIdentity(claims),
                 Expires = DateTime.UtcNow.AddHours(2),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
 
@@ -34,6 +44,49 @@ namespace autenticacao.Services
 
             return tokenHandler.WriteToken(token);
         }
+
+        public static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Settings.Secret)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var secutiryToken);
+
+            if (secutiryToken is not JwtSecurityToken jwtSecurityToken)
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+
+        }
+
+        private static List<Tuple<string, string>> _refreshsTokens = new List<Tuple<string, string>>();
+
+        public static void SaveRefreshToken(string username, string refreshToken)
+            => _refreshsTokens.Add(new Tuple<string, string>(username, refreshToken));
+
+        public static string GetRefreshToken(string username)
+            => _refreshsTokens.FirstOrDefault(x => x.Item1 == username).Item2;
+
+        public static void DeleteRefreshToken(string username, string refreshToken)
+        {
+            var item = _refreshsTokens.FirstOrDefault(x => x.Item1 == username && x.Item2 == refreshToken);
+            _refreshsTokens.Remove(item);
+        }
+
     }
 
 }
